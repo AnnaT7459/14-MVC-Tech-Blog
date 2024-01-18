d7b57f370a49ee0c3f5b2a6463f3c775d4448a00
AnnaT7459 <141446349+AnnaT7459@users.noreply.github.com>
2024-01-17 19:45:33 -0600

    added server, but still needs adjustments, installed nodemon, changed post routes to try/catch statements using module 14 student mini project for reference

--- Controllers/API/Post-routes.js
+++ Controllers/API/Post-routes.js
@@ -1,141 +1,262 @@
-const router = require("express").Router();
-const { Post, User, Comment } = require("../../models");
-const withAuth = require("../../Utils/auth");
+// const router = require("express").Router();
+// const { Post, User, Comment } = require("../../models");
+// const withAuth = require("../../Utils/auth");
 
-// all blog posts route
-router.get("/", (req, res) => {
-  Post.findAll({
-    attributes: [
-      "id",
-      "title",
-      "post_content",
-      "user_id",
-      "date_created",
-      "date_updated",
-    ],
-    order: [["date_created", "DESC"]],
-    include: [
-      {
-        model: Comment,
-        attributes: [
-          "id",
-          "comment_content",
-          "date_created",
-          "user_id",
-          "post_id",
-        ],
-        include: {
+// // all blog posts route
+// router.get("/", (req, res) => {
+//   Post.findAll({
+//     attributes: [
+//       "id",
+//       "title",
+//       "post_content",
+//       "user_id",
+//       "date_created",
+//       "date_updated",
+//     ],
+//     order: [["date_created", "DESC"]],
+//     include: [
+//       {
+//         model: Comment,
+//         attributes: [
+//           "id",
+//           "comment_content",
+//           "date_created",
+//           "user_id",
+//           "post_id",
+//         ],
+//         include: {
+//           model: User,
+//           attributes: ["name"],
+//         },
+//       },
+//       {
+//         model: User,
+//         attributes: ["name"],
+//       },
+//     ],
+//   })
+//     .then((allPostData) => res.json(allPostData))
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+// });
+
+// // single blog post route
+// router.get("/:id", (req, res) => {
+//   Post.findOne({
+//     where: {
+//       id: req.params.id,
+//     },
+//     attributes: [
+//       "id",
+//       "title",
+//       "post_content",
+//       "user_id",
+//       "date_created",
+//       "date_updated",
+//     ],
+//     include: [
+//       {
+//         model: User,
+//         attribtues: ["name"],
+//       },
+//     ],
+//   })
+//     .then((onePostData) => {
+//       if(!onePostData);
+//       {
+//         res.status(404).json({ message: "Unable to find post" });
+//         return;
+//       } 
+//       res.json(onePostData);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+// });
+
+// // create a blog post route
+// router.post("/", withAuth, (req, res) => {
+//   console.log(req.body);
+//   Post.create({
+//     title: req.body.title,
+//     post_content: req.body.post_content,
+//     user_id: req.session.user_id,
+//   }).then((newPostData) => {
+//     res.json(newPostData).catch((err) => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+//   });
+// });
+
+// // update a blog post route
+// router.put("/:id", withAuth, (req, res) => {
+//   Post.update(
+//     {
+//       title: req.body.title,
+//       post_content: req.body.post_content,
+//     },
+//     {
+//       where: {
+//         id: req.params.id,
+//       },
+//     }
+//   )
+//     .then((updatePostData) => {
+//       if (!updatePostData) {
+//         res.status(404).json({ message: "Unable to find post" });
+//         return;
+//       }
+//       res.json(updatePostData);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+// });
+
+// // delete a blog post route
+// router.delete("/:id", withAuth, (req, res) => {
+//   Post.destroy({
+//     where: {
+//       id: req.params.id,
+//     },
+//   })
+//     .then((deletePostData) => {
+//       if (!deletePostData) {
+//         res.status(404).json({ message: "Unable to find post" });
+//         return;
+//       }
+//       res.json(deletePostData);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+// });
+
+// module.exports = router;
+
+
+const router = require('express').Router();
+const { Post } = require('../../models');
+
+
+// get all blog posts (recoded as try/catch statement/exception)
+router.get("/", async (req, res) => {
+  try {
+    const allPostData = await Post.findAll({
+      attributes: [
+        "id",
+        "title",
+        "post_content",
+        "user_id",
+        "date_created",
+        "date_updated",
+      ],
+      order: [["date_created", "DESC"]],
+      include: [
+        {
+          model: Comment,
+          attributes: [
+            "id",
+            "comment_content",
+            "date_created",
+            "user_id",
+            "post_id",
+          ],
+          include: {
+            model: User,
+            attributes: ["name"],
+          },
+        },
+        {
           model: User,
           attributes: ["name"],
         },
-      },
-      {
-        model: User,
-        attributes: ["name"],
-      },
-    ],
-  })
-    .then((allPostData) => res.json(allPostData))
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
+      ],
     });
+    res.json(allPostData);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
 });
 
-// single blog post route
-router.get("/:id", (req, res) => {
-  Post.findOne({
-    where: {
-      id: req.params.id,
-    },
-    attributes: [
-      "id",
-      "title",
-      "post_content",
-      "user_id",
-      "date_created",
-      "date_updated",
-    ],
-    include: [
-      {
-        model: User,
-        attribtues: ["name"],
-      },
-    ],
-  })
-    .then((onePostData) => {
-      if(!onePostData);
-      {
-        res.status(404).json({ message: "Unable to find post" });
-        return;
-      } 
-      res.json(onePostData);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
-
-// create a blog post route
-router.post("/", withAuth, (req, res) => {
-  console.log(req.body);
-  Post.create({
-    title: req.body.title,
-    post_content: req.body.post_content,
-    user_id: req.session.user_id,
-  }).then((newPostData) => {
-    res.json(newPostData).catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-  });
-});
-
-// update a blog post route
-router.put("/:id", withAuth, (req, res) => {
-  Post.update(
-    {
-      title: req.body.title,
-      post_content: req.body.post_content,
-    },
-    {
+// get one blog post by id (recoded as try/catch statement/exception)
+router.get("/:id", async (req, res) => {
+  try {
+    const onePostData = await Post.findOne({
       where: {
         id: req.params.id,
       },
-    }
-  )
-    .then((updatePostData) => {
-      if (!updatePostData) {
-        res.status(404).json({ message: "Unable to find post" });
-        return;
-      }
-      res.json(updatePostData);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
+      attributes: [
+        "id",
+        "title",
+        "post_content",
+        "user_id",
+        "date_created",
+        "date_updated",
+      ],
+      include: [
+        {
+          model: User,
+          attributes: ["name"],
+        },
+      ],
     });
+
+    if (!onePostData) {
+      res.status(404).json({ message: "Unable to find post" });
+      return;
+    }
+
+    res.json(onePostData);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
 });
 
-// delete a blog post route
-router.delete("/:id", withAuth, (req, res) => {
-  Post.destroy({
-    where: {
-      id: req.params.id,
-    },
-  })
-    .then((deletePostData) => {
-      if (!deletePostData) {
-        res.status(404).json({ message: "Unable to find post" });
-        return;
-      }
-      res.json(deletePostData);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
+
+
+// update a blog post route - 14 Student Mini Project for reference
+router.post('/', async (req, res) => {
+  try {
+    const newPost = await Post.create({
+      ...req.body,
+      user_id: req.session.user_id,
     });
+
+    res.status(200).json(newPost);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+
+// delete a blog post route - 14 Student Mini Project for reference
+router.delete('/:id', async (req, res) => {
+  try {
+    const postData = await Post.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!postData) {
+      res.status(404).json({ message: 'Unable to find post!' });
+      return;
+    }
+
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
