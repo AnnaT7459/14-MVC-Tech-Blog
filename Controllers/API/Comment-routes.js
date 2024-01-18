b1db6d26418ee323c7520e0ebb5994cea5ced1c9
AnnaT7459 <141446349+AnnaT7459@users.noreply.github.com>
2024-01-17 19:54:51 -0600

    changed comment route to a try/catch statement/exception

--- Controllers/API/Comment-routes.js
+++ Controllers/API/Comment-routes.js
@@ -3,21 +3,38 @@ const { Comment } = require("../../models");
 const withAuth = require("../../Utils/auth");
 
 // create a comment route
-router.post("/", withAuth, (req, res) => {
-    if (req.session) {
-        Comment.create({
-            comment_content: req.body.comment_content,
-            user_id: req.session.user_id,
-            post_id: req.body.post_id,
-        })
-        .then((newCommentData) => {
-            res.json(newCommentData)
-            .catch((err) => {
-                console.log(err);
-                res.status(500).json(err);
-            });
-        });
-    }
-})
+// router.post("/", withAuth, (req, res) => {
+//     if (req.session) {
+//         Comment.create({
+//             comment_content: req.body.comment_content,
+//             user_id: req.session.user_id,
+//             post_id: req.body.post_id,
+//         })
+//         .then((newCommentData) => {
+//             res.json(newCommentData)
+//             .catch((err) => {
+//                 console.log(err);
+//                 res.status(500).json(err);
+//             });
+//         });
+//     }
+// })
 
+// create a comment route - changed to try/catch statement/exception
+router.post("/", withAuth, async (req, res) => {
+    try {
+      if (req.session) {
+        const newCommentData = await Comment.create({
+          comment_content: req.body.comment_content,
+          user_id: req.session.user_id,
+          post_id: req.body.post_id,
+        });
+        res.json(newCommentData);
+      }
+    } catch (err) {
+      console.error(err);
+      res.status(500).json(err);
+    }
+  });
+  
 module.exports = router
