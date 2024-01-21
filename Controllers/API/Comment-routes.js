af70ce40b5d36cfacf3caebdb32a9a2c8db5069c
AnnaT7459 <141446349+AnnaT7459@users.noreply.github.com>
2024-01-20 18:14:45 -0600

    deleted and cleaned up old code from post and comment routes

--- Controllers/API/Comment-routes.js
+++ Controllers/API/Comment-routes.js
@@ -2,24 +2,6 @@ const router = require("express").Router();
 const { Comment } = require("../../models");
 const withAuth = require("../../Utils/auth");
 
-// create a comment route
-// router.post("/", withAuth, (req, res) => {
-//     if (req.session) {
-//         Comment.create({
-//             comment_content: req.body.comment_content,
-//             user_id: req.session.user_id,
-//             post_id: req.body.post_id,
-//         })
-//         .then((newCommentData) => {
-//             res.json(newCommentData)
-//             .catch((err) => {
-//                 console.log(err);
-//                 res.status(500).json(err);
-//             });
-//         });
-//     }
-// })
-
 // create a comment route - changed to try/catch statement/exception
 router.post("/", withAuth, async (req, res) => {
     try {
