2465715e7c7135a6f87c470e797490ba4fd15cfa
AnnaT7459 <141446349+AnnaT7459@users.noreply.github.com>
2024-01-20 18:01:55 -0600

    updated home routes by pulling from post routes, logic to render content

--- Controllers/Homepage-routes.js
+++ Controllers/Homepage-routes.js
@@ -1,7 +1,94 @@
 const router = require("express").Router();
 const { Post, User, Comment } = require("../../models");
 const withAuth = require("../../Utils/auth");
-const sequelize = require("../Config/Connnection");
+
+
+// sign up
+router.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
+// get all blog posts (recoded as try/catch statement/exception)
+router.get("/", async (req, res) => {
+  try {
+    const allPostData = await Post.findAll({
+      attributes: [
+        "id",
+        "title",
+        "post_content",
+        "user_id",
+        "date_created",
+        "date_updated",
+      ],
+      order: [["date_created", "DESC"]],
+      include: [
+        {
+          model: Comment,
+          attributes: [
+            "id",
+            "comment_content",
+            "date_created",
+            "user_id",
+            "post_id",
+          ],
+          include: {
+            model: User,
+            attributes: ["name"],
+          },
+        },
+        {
+          model: User,
+          attributes: ["name"],
+        },
+      ],
+    });
+
+    const posts = allPostData.map((post) => post.get({ plain: true}));
+
+    // session
+    res.render("homepage", {
+      posts,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+});
+
+
+
+// get one blog post by id (recoded as try/catch statement/exception)
+router.get("/post/:id", async (req, res) => {
+  try {
+    const onePostData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ["name"],
+        },
+        {model: Comment,
+        attributes: [id, comment_content, user_id],
+      }
+      ],
+    });
+
+    const post = onePostData.get({ plain: true })
+    res.render("post", {
+      ...post,
+      logged_in: req.session.logged_in,
+      userID: req.session.user_id,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+//   new blog post option on dashboard
+router.get("/new", (req, res) => {
+  res.render("new-post", { name: req.session.name });
+});
 
 // login
 router.get("/login", (req, res) => {
@@ -10,62 +97,6 @@ router.get("/login", (req, res) => {
   } else {
     res.render("login");
   }
-});
-
-// sign up
-router.get("/signup", (req, res) => {
-  res.render("signup");
-});
-
-// all blog posts route
-router.get("/", (req, res) => {
-  Post.findAll({
-    where: {
-      user_id: req.session.user_id,
-    },
-    attributes: [
-      "id",
-      "title",
-      "post_content",
-      "user_id",
-      "date_created",
-      "date_updated",
-    ],
-    order: [["date_created", "DESC"]],
-    include: [
-      {
-        model: Comment,
-        attributes: [
-          "id",
-          "comment_content",
-          "date_created",
-          "user_id",
-          "post_id",
-        ],
-        include: {
-          model: User,
-          attributes: ["name"],
-        },
-      },
-      {
-        model: User,
-        attributes: ["name"],
-      },
-    ],
-  })
-    .then((allPostData) => {
-      const userPosts = allPostData.map((post) => post.get({ plain: true }));
-      res.render("dashboard", { userPosts, logged_in: true });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
-
-//   new blog post option on dashboard
-router.get("/new", (req, res) => {
-  res.render("new-post", { name: req.session.name });
 });
 
 module.exports = router;
