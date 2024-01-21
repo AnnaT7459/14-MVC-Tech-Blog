398224f492bd480d8c75ecb8c2130de7624b5693
AnnaT7459 <141446349+AnnaT7459@users.noreply.github.com>
2024-01-20 18:21:55 -0600

    rearranged and debugged index model

--- Models/index.js
+++ Models/index.js
@@ -4,14 +4,20 @@ const Comment = require("./Comment");
 
 User.hasMany(Post, {
   foreignKey: "user_id",
+  onDelete: "CASCADE"
+});
+
+Post.belongsTo(User, {
+  foreignKey: "user_id",
 });
 
 User.hasMany(Comment, {
   foreignKey: "user_id",
+  onDelete: "CASCADE"
 });
 
-Post.belongsTo(User, {
-  foreignKey: "post_id",
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
 });
 
 Post.hasMany(Comment, {
@@ -19,13 +25,8 @@ Post.hasMany(Comment, {
   onDelete: "CASCADE",
 });
 
-Comment.belongsTo(User, {
-  foreignKey: "user_id",
-});
-
 Comment.belongsTo(Post, {
   foreignKey: "post_id",
-  onDelete: "CASCADE",
 });
 
 module.exports = { User, Post, Comment };
